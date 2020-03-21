d330da66c8d9d713e1b63baff0ccf7dbe8ab3e29
Julian Aßmann <assmannjulian@outlook.de>
2020-03-21 13:02:03 +0100

    Added database connection status to status api endpoint

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,9 +2,10 @@ import {createConnection, ConnectionOptions} from 'typeorm';
 import {Connection} from "typeorm/connection/Connection";
 import { environmentVariables } from '../config/environment.config';
 
+export let databaseOK = false;
 export let connection: Connection;
 export const initializeDatabase = async () => {
-    console.log(environmentVariables.postgresHost)
+    console.log(environmentVariables.postgresHost);
     const options: ConnectionOptions = {
         type: "postgres",
         // url: `postgres://${environmentVariables.postgresUser}:${environmentVariables.postgresPassword}@${environmentVariables.postgresHost}/${environmentVariables.postgresDb}`,
@@ -20,9 +21,11 @@ export const initializeDatabase = async () => {
     try {
         connection = await createConnection(options);
         console.log('✔️ Connection to Database established.');
+        databaseOK = true;
 
     } catch (error) {
         console.log('❌ Error: Could not connect to Database');
         console.log(error);
+        databaseOK = false;
     }
 };
--- src/handler/status.ts
+++ src/handler/status.ts
@@ -2,6 +2,7 @@ import {router} from "../index";
 import express from "express"
 import {wrap} from "../errorHandler";
 import {authGuard} from "../middleware/authGuard.middleware";
+import {databaseOK} from '../database/database';
 
 export default class StatusHandler {
     async initialize() {
@@ -11,7 +12,11 @@ export default class StatusHandler {
     }
 
     async getStatus(req: express.Request, res: express.Response) {
-        res.send({status: "OK"})
+        let ret = {
+            status: "OK",
+            databaseStatus: databaseOK ? 'OK' : 'Error',
+        }
+        res.send(ret)
     }
 
     async getAuth(req: express.Request, res: express.Response) {
