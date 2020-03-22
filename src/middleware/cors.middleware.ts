17f1b4200807f30f15ca1206e2df669a136b393e
Felix Hromadko <felix@hromadko.at>
2020-03-22 15:54:13 +0100

    Cors Allow Content-Type

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -6,8 +6,7 @@ export function registerCors() {
     const corsMiddleware = cors({
         origin: process.env.NODE_ENV === 'production' ? 'https://getjay.fourfive.studio' : ['localhost', 'localhost:3000', 'localhost:3001'],
         methods: ['GET', 'POST', 'DELETE'],
-        allowedHeaders: ['Authorization'],
-
+        allowedHeaders: ['Authorization', 'content-type'],
     });
 
     app.use(corsMiddleware);
