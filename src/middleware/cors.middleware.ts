820641835ab841ee63d2f3350bea773c8c4fc189
Felix Hromadko <felix@hromadko.at>
2020-03-22 12:36:10 +0100

    cors

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -4,15 +4,15 @@ import {app} from "../index";
 
 export function registerCors() {
     const corsMiddleware = cors({
-        origin: process.env.NODE_ENV === 'production' ? 'getjay.fourfive.studio' : ['localhost', 'localhost:3000', 'localhost:3001'],
+        origin: process.env.NODE_ENV === 'production' ? 'https://getjay.fourfive.studio' : ['localhost', 'localhost:3000', 'localhost:3001'],
         methods: ['GET', 'POST', 'DELETE'],
         allowedHeaders: ['Authorization'],
 
     });
-    app.options('*', (req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://getjay.fourfive.studio');
-        next();
-    });
+    //app.options('*', (req, res, next) => {
+    //    res.setHeader('Access-Control-Allow-Origin', 'https://getjay.fourfive.studio');
+    //    next();
+    //});
     app.use(corsMiddleware);
     app.options('*', corsMiddleware)
 }
